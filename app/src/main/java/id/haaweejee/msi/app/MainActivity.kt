433cee0a17c65e2172e89bdb6dd3044725c71f88
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-06-29 22:57:39 +0700

    + connected viewmodel with ui

--- app/src/main/java/id/haaweejee/msi/app/MainActivity.kt
+++ app/src/main/java/id/haaweejee/msi/app/MainActivity.kt
@@ -2,10 +2,31 @@ package id.haaweejee.msi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import id.haaweejee.msi.app.databinding.ActivityMainBinding
+import id.haaweejee.msi.app.ui.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding : ActivityMainBinding
+    private lateinit var _viewModel : MovieViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        initializeViewModel()
+
+        _viewModel.getDiscover()
+        _viewModel.discover.observe(this){ discover ->
+            _binding.tv.text = discover.toString()
+        }
+
+
+    }
+
+
+    private fun initializeViewModel(){
+        _viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
     }
 }
