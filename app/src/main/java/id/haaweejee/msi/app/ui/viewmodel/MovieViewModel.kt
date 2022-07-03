2f11d111f58a56cdd283d0e2cc76fbadb357751c
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-07-04 00:46:53 +0700

    ! refactor viewmodel

--- app/src/main/java/id/haaweejee/msi/app/ui/viewmodel/MovieViewModel.kt
+++ app/src/main/java/id/haaweejee/msi/app/ui/viewmodel/MovieViewModel.kt
@@ -1,4 +1,4 @@
-package id.haaweejee.msi.app.ui
+package id.haaweejee.msi.app.ui.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.LiveData
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import id.haaweejee.msi.app.core.data.remote.api.RetrofitClient
 import id.haaweejee.msi.app.core.data.remote.model.movie.MovieResponse
+import id.haaweejee.msi.app.core.data.remote.model.series.SeriesResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -14,14 +15,31 @@ class MovieViewModel : ViewModel() {
     private val _discover = MutableLiveData<MovieResponse>()
     val discover : LiveData<MovieResponse> = _discover
 
+    private val _discoverSeries = MutableLiveData<SeriesResponse>()
+    val discoverSeries : LiveData<SeriesResponse> = _discoverSeries
+
     fun getDiscover(){
         viewModelScope.launch(Dispatchers.IO){
             try {
-                val client = RetrofitClient.apiInstance.getDiscover()
+                val client = RetrofitClient.getApiService().getDiscover()
                 if (client.isSuccessful){
                     _discover.postValue(client.body())
                     Log.d("MovieViewModel", client.body().toString())
 
+                }
+            }catch (ex : Exception){
+                Log.d("MovieViewModel", ex.message.toString())
+            }
+        }
+    }
+
+    fun getDiscoverSeries(){
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                val client = RetrofitClient.getApiService().getDiscoverSeries()
+                if (client.isSuccessful){
+                    _discoverSeries.postValue(client.body())
+                    Log.d("MovieViewModel", client.body().toString())
                 }
             }catch (ex : Exception){
                 Log.d("MovieViewModel", ex.message.toString())
