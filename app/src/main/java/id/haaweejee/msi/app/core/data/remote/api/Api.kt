bbda674498b865068d059dd43ba4282593f13fed
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-07-04 00:42:48 +0700

    ! update endpoint

--- app/src/main/java/id/haaweejee/msi/app/core/data/remote/api/Api.kt
+++ app/src/main/java/id/haaweejee/msi/app/core/data/remote/api/Api.kt
@@ -2,6 +2,7 @@ package id.haaweejee.msi.app.core.data.remote.api
 
 import id.haaweejee.msi.app.BuildConfig
 import id.haaweejee.msi.app.core.data.remote.model.movie.MovieResponse
+import id.haaweejee.msi.app.core.data.remote.model.series.SeriesResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -13,4 +14,11 @@ interface Api {
         @Query("language") language: String= BuildConfig.ENGLISH_US,
         @Query("page") page: Int = 1
     ): Response<MovieResponse>
+
+    @GET("discover/tv")
+    suspend fun getDiscoverSeries(
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_TOKEN,
+        @Query("language") language: String= BuildConfig.ENGLISH_US,
+        @Query("page") page: Int = 1
+    ): Response<SeriesResponse>
 }
