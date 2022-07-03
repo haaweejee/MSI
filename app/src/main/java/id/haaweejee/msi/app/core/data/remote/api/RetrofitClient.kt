08c803458c7df04c3d1c7eadeeb273d0696b1fc6
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-07-04 00:45:20 +0700

    ! refactor gson to moshi converter

--- app/src/main/java/id/haaweejee/msi/app/core/data/remote/api/RetrofitClient.kt
+++ app/src/main/java/id/haaweejee/msi/app/core/data/remote/api/RetrofitClient.kt
@@ -1,19 +1,24 @@
 package id.haaweejee.msi.app.core.data.remote.api
 
+import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private const val BASE_URL = "https://api.themoviedb.org/3/"
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(getLoggingIntreceptor())
-        .build()
+    fun getApiService() : Api{
+        val moshi = Moshi.Builder().build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(getLoggingIntreceptor())
+            .build()
+        return retrofit.create(Api::class.java)
+    }
 
     fun getLoggingIntreceptor(): OkHttpClient {
         return OkHttpClient.Builder()
@@ -22,7 +27,4 @@ object RetrofitClient {
             .readTimeout(120, TimeUnit.SECONDS)
             .build()
     }
-
-    val apiInstance:Api = retrofit.create(Api::class.java)
-
 }
