303598a1e7456c004a86e41050bb2e31913221c5
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-07-28 16:27:29 +0700

    ! update viewmodel

--- app/src/main/java/id/haaweejee/msi/app/ui/viewmodel/MovieViewModel.kt
+++ app/src/main/java/id/haaweejee/msi/app/ui/viewmodel/MovieViewModel.kt
@@ -1,49 +1,15 @@
 package id.haaweejee.msi.app.ui.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import id.haaweejee.msi.app.core.data.remote.api.RetrofitClient
-import id.haaweejee.msi.app.core.data.remote.model.movie.MovieResponse
-import id.haaweejee.msi.app.core.data.remote.model.series.SeriesResponse
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import androidx.lifecycle.asLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import id.haaweejee.msi.app.core.domain.usecase.MovieSeriesUseCase
+import javax.inject.Inject
 
-class MovieViewModel : ViewModel() {
-    private val _discover = MutableLiveData<MovieResponse>()
-    val discover : LiveData<MovieResponse> = _discover
+@HiltViewModel
+class MovieViewModel @Inject constructor(useCase: MovieSeriesUseCase) : ViewModel() {
 
-    private val _discoverSeries = MutableLiveData<SeriesResponse>()
-    val discoverSeries : LiveData<SeriesResponse> = _discoverSeries
+   val movie = useCase.discoverMovie().asLiveData()
+   val series = useCase.discoverSeries().asLiveData()
 
-    fun getDiscover(){
-        viewModelScope.launch(Dispatchers.IO){
-            try {
-                val client = RetrofitClient.getApiService().getDiscover()
-                if (client.isSuccessful){
-                    _discover.postValue(client.body())
-                    Log.d("MovieViewModel", client.body().toString())
-
-                }
-            }catch (ex : Exception){
-                Log.d("MovieViewModel", ex.message.toString())
-            }
-        }
-    }
-
-    fun getDiscoverSeries(){
-        viewModelScope.launch(Dispatchers.IO){
-            try {
-                val client = RetrofitClient.getApiService().getDiscoverSeries()
-                if (client.isSuccessful){
-                    _discoverSeries.postValue(client.body())
-                    Log.d("MovieViewModel", client.body().toString())
-                }
-            }catch (ex : Exception){
-                Log.d("MovieViewModel", ex.message.toString())
-            }
-        }
-    }
 }
